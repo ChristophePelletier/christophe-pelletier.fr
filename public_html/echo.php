a9f1e8fffd94cbe3331efea44dc98f8156621db3
ChristophePelletier <christophe.pelletier.contact@gmail.com>
2022-05-25 16:10:52 +0200

    save

---
+++ public_html/echo.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html><head><meta charset="utf-8"></head>
+<body>
+    <?php echo __DIR__;?>
+    <?php include __DIR__ . '/create-table.php';?>
+    <?php echo $output; ?>
+
+</body></html>
